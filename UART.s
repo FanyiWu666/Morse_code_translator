5aa9f226ca4bfbda6dbf23b5163ac7e5f82a3103
fw1120 <fw1120@skbl-422-007.ph.ic.ac.uk>
2022-12-09 11:05:08 +0000

    project config update

--- UART.s
+++ UART.s
@@ -1,16 +1,18 @@
 #include <xc.inc>
     
-global  UART_Setup, UART_Transmit_Message, UART_Load_Byte
+global  UART_Setup, UART_Transmit_Message, UART_Load_Byte, UART_Transmit_Byte, result
 
 psect	udata_acs   ; reserve data space in access ram
 UART_counter: ds    1	    ; reserve 1 byte for variable UART_counter
-
+result: ds	    1 ; reserve 1 byte for variable UART_input
+    
 psect	uart_code,class=CODE
 UART_Setup:
     bsf	    SPEN	; enable
     bcf	    SYNC	; synchronous
     bcf	    BRGH	; slow speed
     bsf	    TXEN	; enable transmit
+    bsf	    CREN	; enable receive
     bcf	    BRG16	; 8-bit generator only
     movlw   103		; gives 9600 Baud rate (actually 9615)
     movwf   SPBRG1, A	; set baud rate
@@ -21,20 +23,32 @@ UART_Setup:
 UART_Transmit_Message:	    ; Message stored at FSR2, length stored in W
     movwf   UART_counter, A
 UART_Loop_message:
-    movf    POSTINC2, W, A
+    movf    POSTINC2, W, A ; ;Accsess the value in FSR2 and automatically increaments it by '1' afterwards, and move the value to W
     call    UART_Transmit_Byte
     decfsz  UART_counter, A
     bra	    UART_Loop_message
     return
-
+    
 UART_Transmit_Byte:	    ; Transmits byte stored in W
     btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
     bra	    UART_Transmit_Byte
     movwf   TXREG1, A
     return
 
-UART_Load_Byte:
-    btfss   RC1IF
-    bra	    UART_Load_Byte
-    movf    RCREG1, W, A
+UART_Load_Byte:	    ; Loads RECEIVED byte to W
+    btfss   RC1IF	    ; RC1IF is set when TXREG1 is empty
+    ;bra	    UART_Load_Byte
+    movf    RCREG1, W ;READ 8-BIT DATA ONLY
+    movwf   result
+    movlw   0x00
+    movwf   RCREG1
+    return   
+        
+    ;NEED TO SPLIT THE INPUT MESSAGE 
+    ;NEED TO CREATE A NEW ADDRESS MENMORY STORING ENOUGH INPUT CHARACTERS
+Loop_Load_message: ; Message stored at RCREG1
+    movf    RCREG1, W, A ;Accsess the value in FSR2 and automatically increaments it by '1' afterwards, and move the value to W
+    call    UART_Load_Byte
+    decfsz  UART_counter, A
+    bra	    Loop_Load_message
     return
