f9843dc6a545c3d72929630db6d7aafed2ca3724
fw1120 <fw1120@skbl-422-007.ph.ic.ac.uk>
2022-12-09 11:05:43 +0000

    project config update

--- main.s
+++ main.s
@@ -1,23 +1,23 @@
 #include <xc.inc>
 
-extrn     UART_Setup, UART_Transmit_Message, UART_Load_Byte, UART_Transmit_Byte  ; external subroutines
-extrn     LCD_Setup, LCD_Write_Message
-extrn     Encode_start
-                
-psect     udata_acs   ; reserve data space in access ram
+extrn	UART_Setup, UART_Transmit_Message, UART_Load_Byte, UART_Transmit_Byte  ; external subroutines
+extrn	LCD_Setup, LCD_Write_Message
+extrn	Encode_start
+	
+psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
    
-psect     code, abs             
-rst:         org 0x0
-               goto       setup
+psect	code, abs	
+rst: 	org 0x0
+ 	goto	setup
 
-                ; ******* Programme FLASH read Setup Code ***********************
-setup:   bcf          CFGS     ; point to Flash program memory  
-                bsf          EEPGD   ; access Flash program memory
-                call          UART_Setup      ; setup UART
-                call          LCD_Setup          ; setup UART
-                goto       start
+	; ******* Programme FLASH read Setup Code ***********************
+setup:	bcf	CFGS	; point to Flash program memory  
+	bsf	EEPGD 	; access Flash program memory
+	call	UART_Setup	; setup UART
+	call	LCD_Setup	; setup LCD
+	goto	start
 
 	; ******* Main programme ****************************************
 start:	call	UART_Transmit_Byte
@@ -38,12 +38,13 @@ Encode_test:	call	Encode_start
 	
 LCD_test:       call	LCD_Write_Message
 		;bra	LCD_test
-                
-                
-                
+		
 
-delay:    decfsz   delay_count, A  ; decrement until zero
-                bra         delay
-                return
-                
-                end        rst
+	       
+	
+	
+delay:	decfsz	delay_count, A	; decrement until zero
+	bra	delay
+	return
+	
+	end	rst
