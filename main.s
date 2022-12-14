93e866017ae32592bd9a24b337b2d5819e992509
fw1120 <fw1120@skbl-422-007.ph.ic.ac.uk>
2022-12-14 16:38:56 +0000

    add delay

--- main.s
+++ main.s
@@ -4,7 +4,12 @@ extrn	UART_Setup, UART_Load_Byte, UART_Transmit_Byte  ; external subroutines
 extrn	LCD_Setup, LCD_clear_display
 extrn	Encode_start
 extrn	LED_start
-   
+
+psect	udata_acs   ; reserve data space in access ram
+d1:ds 1    ; reserve one byte for counter in the delay routine
+d2:ds 1    ; reserve one byte for counter in the delay routine
+d3:ds 1    ; reserve one byte for counter in the delay routine
+    
 psect	code, abs	
 rst: 	org 0x0
  	goto	setup
@@ -28,5 +33,23 @@ test:      call          UART_Load_Byte
             
            goto       $                              ; goto current line in code   
 
+delay:	
+    movlw 0x0B
+    movwf d1, A
+    movlw 0x0B
+    movwf d2, A
+    movlw 0x0B
+    movwf d3, A
+    
+delay_0:
+    decfsz	 d1, f	; decrement until zero
+    bra	delay_1
+    decfsz	d2, f
+delay_1:
+    bra delay_2
+    decfsz	d3, f
+delay_2:
+    bra delay_0
+    return
 	
 	end	rst
